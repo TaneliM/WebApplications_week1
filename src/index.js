f248da5d856a60570f7e6f51308d16884955f174
TaneliM <64307246+TaneliM@users.noreply.github.com>
2021-09-14 12:56:46 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+document.getElementById("menu").innerHTML = `
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Price</th>
+  </tr>
+  <tr>
+    <td>Meal 1</td>
+    <td>1.00€</td>
+  </tr>
+  <tr>
+    <td>Meal 2</td>
+    <td>2.00€</td>
+  </tr>
+  <tr>
+    <td>Meal 3</td>
+    <td>3.00€</td>
+  </tr>
+  <tr>
+    <td>Meal 4</td>
+    <td>4.00€</td>
+  </tr>
+  <tr>
+    <td>Meal 5</td>
+    <td>5.00€</td>
+  </tr>
+</table>
+`;
